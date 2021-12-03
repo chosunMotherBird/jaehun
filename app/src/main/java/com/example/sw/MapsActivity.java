427bd3d21651cfbe788a608f8fc88b3c7a352925
newjaehun <sjh030798@gmail.com>
2021-12-03 23:32:43 +0900

    수정

--- app/src/main/java/com/example/sw/MapsActivity.java
+++ app/src/main/java/com/example/sw/MapsActivity.java
@@ -1,19 +1,19 @@
 package com.example.sw;
 
+import androidx.fragment.app.FragmentActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.fragment.app.FragmentActivity;
-
-import com.example.sw.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.sw.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -37,7 +37,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MapsActivity.this, com.example.sw.LoginActivity.class);
+                Intent intent=new Intent(MapsActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
@@ -53,10 +53,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
--- app/src/main/java/com/example/sw/SignUpActivity.java
+++ app/src/main/java/com/example/sw/SignUpActivity.java
@@ -1,11 +1,11 @@
 package com.example.sw;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
     private Button sGoBack, sSignUp;
